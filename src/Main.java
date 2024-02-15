9eb9bbd3647b023f92800a7751fef0abf7c4a58d
anstasiagritsenko <76060039+anstasiagritsenko@users.noreply.github.com>
2024-02-15 10:19:05 +0300

    Исправление несоответствия требованиям

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         String[] operators = {"+", "-", "*", "/"};
         String[] regexOperators = {"\\+", "-", "\\*", "/"};
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите математическое выражение:");
         String expression = scanner.nextLine();
 
         int expressionIndex = -1;
@@ -16,19 +14,19 @@ public class Main {
             }
         }
         if (expressionIndex == -1) {
-            System.out.println("Некоррекное выражение");
-            return;
+            throw new IllegalArgumentException("Некорректное выражение");
         }
         String[] numbers = expression.split(regexOperators[expressionIndex]);
+        if (numbers.length > 2) {
+            throw new IllegalArgumentException("Некорректное выражение");
+        }
         int a, b;
         a = Integer.parseInt(numbers[0]);
         b = Integer.parseInt(numbers[1]);
 
         if (a < 1 || a > 10 || b < 1 || b > 10) {
-            System.out.println("Некоррекное выражение");
-            return;
+            throw new IllegalArgumentException("Некорректное выражение");
         }
-
         if (expressionIndex == 0) {
             System.out.println(a + b);
         }
